eb0a47470481709dd82cd846ce1934da145c207b
Doru Cioclea <cioclea.doru@gmail.com>
2017-08-11 16:47:00 +0300

    add claims controller

---
+++ src/App/App.Api/Controllers/ClaimsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace App.Api.Controllers
+{
+    [RoutePrefix("api/claims")]
+    public class ClaimsController : BaseApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult GetClaims()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+
+            var claims = from c in identity?.Claims
+                select new
+                {
+                    subject = c.Subject.Name,
+                    type = c.Type,
+                    value = c.Value
+                };
+
+            return Ok(claims);
+        }
+
+    }
+}
