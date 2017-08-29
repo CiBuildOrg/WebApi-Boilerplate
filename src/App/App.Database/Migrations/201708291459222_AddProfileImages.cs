0d121c78d19bc4e5588e1f508f98ecb2eeb5dfeb
Doru Cioclea <cioclea.doru@gmail.com>
2017-08-29 17:59:51 +0300

    add migration for the profile images

---
+++ src/App/App.Database/Migrations/201708291459222_AddProfileImages.cs
@@ -0,0 +1,35 @@
+namespace App.Database
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddProfileImages : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Images",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        UserProfileId = c.Guid(nullable: false),
+                        ImageType = c.Int(nullable: false),
+                        ImageSize = c.Int(nullable: false),
+                        FileName = c.String(),
+                        MimeType = c.String(),
+                        DateStoredUtc = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.UserProfiles", t => t.UserProfileId, cascadeDelete: true)
+                .Index(t => t.UserProfileId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Images", "UserProfileId", "dbo.UserProfiles");
+            DropIndex("dbo.Images", new[] { "UserProfileId" });
+            DropTable("dbo.Images");
+        }
+    }
+}
