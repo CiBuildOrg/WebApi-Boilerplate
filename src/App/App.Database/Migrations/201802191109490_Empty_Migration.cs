e10ede6e66d49355bf293481e748f078dad4b4ef
cDoru <cioclea.doru@gmail.com>
2018-02-19 15:47:43 +0200

    updates updates updates

---
+++ src/App/App.Database/Migrations/201802191109490_Empty_Migration.cs
@@ -0,0 +1,18 @@
+namespace App.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Empty_Migration : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+           
+        }
+    }
+}
